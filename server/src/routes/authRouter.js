c26746dd286e5c0e47b6efa87734447bc66a8234
Harsh-Raaz <harshrajforofficial@gmail.com>
2025-12-31 20:58:19 +0530

    send user info from backend auth routes

--- server/src/routes/authRouter.js
+++ server/src/routes/authRouter.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { login, register } from "../controllers/authController.js";
+import { login, register, logout, verify } from "../controllers/authController.js";
 
 const authrouter = express.Router();
 
 authrouter.post('/register',register);
 authrouter.post('/login',login);
+authrouter.post('/logout',logout);
+authrouter.get('/verify',verify);
 
 export default authrouter;
